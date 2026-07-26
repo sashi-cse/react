8c3c76f9e9f0a39fc9230e655fbaec13813f7e7c
sashi-cse <sashikant112005@gmail.com>
2026-07-26 15:59:19 +0530

    proj2_counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)
+  // let counter = 15;
+  const addValue = () => {
+    // console.log("clicked", counter);
+    // counter = counter + 1
+    setCounter(counter+1);
+  }
+
+  const removeValue = ()=> {
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>Sashi nd React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button 
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br/>
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
